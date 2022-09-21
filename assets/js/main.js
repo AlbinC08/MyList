bec838b951db61af8d71dc5d9da82e293b7a5156
AlbinC08 <cedric.albin13780@gmail.com>
2022-09-21 14:47:26 +0200

    21-09-22-14h47

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,30 @@ let body = document.querySelector('body')
 let btnDisplayNav = document.querySelector('#displayNav')
 
 document.addEventListener("click", function (event) {
-    if (event.target != prout) {
-        prout.classList.add('d-none');
-        prout.classList.remove('animate__bounceInLeft')
+    if (event.target != (prout || btnDisplayNav)) {
+        prout.classList.remove('animate__slideInLeft')
+        prout.classList.add('animate__slideOutLeft')
+        
     }
 })
 
 document.querySelector('#displayNav').addEventListener('click', function (event) {
     leftStart()
     event.stopPropagation()
+    prout.classList.remove('d-none');
+
 })
 
 function leftStart() {
     if (prout.classList.contains('d-none')) {
+        console.log("in");
         prout.classList.remove('d-none');
-        prout.classList.add('animate__bounceInLeft')
-    }else{
-        prout.classList.add('d-none');
-        prout.classList.remove('animate__bounceInLeft')
+        prout.classList.add('animate__slideInLeft')
+    }else if (prout.classList.contains('animate__slideOutLeft')) {
+        console.log("out");
+        prout.classList.remove('animate__slideOutLeft')
+        prout.classList.add('animate__slideInLeft')
+
     }
 }
 
@@ -29,3 +35,5 @@ function sendForm(img) {
     document.querySelector('#ListPicture').value = data
     document.querySelector('form').submit()
 }
+
+
